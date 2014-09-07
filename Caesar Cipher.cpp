8301d1128f63fcdb0b3eb3321b33f5efbaa9df49
Whiskeycourage <jeffreyvluong@gmail.com>
2014-09-07 08:35:29 -0700

    Folder Cleanup
    Renamed assignment files and deleted trash files

---
+++ Caesar Cipher.cpp
@@ -0,0 +1,49 @@
+/******************************************************
+* asst03.cpp - Create a Caesar Cipher
+* max25  Jeffrey Luong
+* Tues Jul  8  7:42:00:00 PDT 2014
+******************************************************/
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+int main (int argc, char *argv[])
+{
+  char letter; 
+  string words = "";
+  int shift = atoi(argv[1]);
+    
+  
+      while (! cin.eof())
+      {
+        letter = cin.get();
+        words += letter;
+        if (letter == '\n')
+        {
+            for (int a = 0; a < words.length(); a++)
+            {
+                if (isspace(words[a]))
+                  continue;
+              
+              if (isalpha(words[a]))
+              {
+                if (isupper(words[a]))
+                {
+                  words[a] = tolower(words[a]);
+                  words[a] = (((words[a]-97)+shift)%26)+97;
+                }
+                else
+                {
+                  words[a] = (((words[a]-97)+shift)%26)+97;
+                }  
+              }
+            }
+         cout << words <<endl;
+         words = ""; 
+        }
+      }
+      
+
+}
+        
---
+++ Years to days hours minutes seconds converter.cpp
@@ -0,0 +1,30 @@
+/***********************************************************
+* asst0.cpp - Create a program that converts years to days,
+* hours, mins, and seconds.
+* max25 / Jeffrey Luong
+* Fri Jun 13 8:00:00 PDT 2014
+************************************************************/
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    float years, days, hours, min, sec;
+    cout << "How many years have you played The Game: ";
+    cin >> years;
+    days = years * 365.25;
+    hours = days * 24;
+    min = hours * 60;
+    sec = min * 60;
+    cout << "You have played: " <<years << " Years\n";
+    cout << days <<" Days\n";
+    cout << hours << " Hours\n";
+    cout << min << " Minutes\n";
+    cout << sec << " Seconds\n";
+    return 0;
+}
